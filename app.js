62e2f700c64e3adfb0f652f3e2da4de53609245b
Samuel <b9007732@newcastle.ac.uk>
2021-01-07 03:17:35 +0000

    separated navbar files and included them in all pug views file

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var transfersRouter = require('./routes/transfers');
 var loginRouter = require('./routes/login');
 var newLoginRouter = require('./routes/new_login');
 var signUpRouter = require('./routes/sign_up');
+var contactUsRouter = require('./routes/contact_us');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use('/transfers', transfersRouter);
 app.use('/login', loginRouter);
 app.use('/new_login', newLoginRouter);
 app.use('/sign_up', signUpRouter);
+app.use('/contact_us', contactUsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
