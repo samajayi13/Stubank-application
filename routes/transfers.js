dc408aecf4c1b5de1375eb72a80dba22e0962110
lappp <b9023686@newcastle.ac.uk>
2021-01-12 17:09:42 +0000

    .

--- routes/transfers.js
+++ routes/transfers.js
@@ -7,4 +7,10 @@ router.get('/', function(req, res, next) {
     res.render('transfers', { title: 'Transfers' });
 });
 
+let db = require('../dbconnection');
+
+//db.query("SELECT Transfer_type, ID, Amount_sent, Date_Of_Transfer FROM Transfers_Information JOIN Transfers ON Transfer_From_ID OR Transfer_To_ID = userID )
+
+db.endConnection();
+
 module.exports = router;
