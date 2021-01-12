6d72eb26b326328713854ed7e446e096f6a0fb53
Samuel <b9007732@newcastle.ac.uk>
2021-01-12 15:40:06 +0000

    payment pug changed to remove multiple form tags

--- routes/payment.js
+++ routes/payment.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+var db =
 /* GET payment page. */
 router.get('/', function(req, res, next) {
     res.render('payment', { title: 'Payment' });
+});
+
+
+let db = require('../dbconnection');
+
+router.post('/createPayment', function(req, res, next) {
+
 });
 
 module.exports = router;
