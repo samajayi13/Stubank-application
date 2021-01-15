22370007b39e7aff1a5e00f1cd9fb2b48fdf8baf
Ivelina <i.kiryakova1@newcastle.ac.uk>
2021-01-15 14:01:20 +0200

    Cannot access account page if not logged in

--- routes/account.js
+++ routes/account.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+// if not logged in, doesn't display account page
+const redirectToLogin = (req, res, next) => {
+    if (!req.session.username) {
+        res.redirect('/login')
+    } else {
+        next()
+    }
+}
+
+// displays account page
+router.get('/', redirectToLogin, function(req, res, next) {
     res.render('account', { title: 'Account' });
 });
 
