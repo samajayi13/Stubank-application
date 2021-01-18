768c21edec3475e286586278eed48e38c29e7c04
Samuel Ajayi <ohajayi13@gmail.com>
2021-01-18 23:30:26 +0000

    encryption updated

--- routes/sign_up.js
+++ routes/sign_up.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var CryptoJS = require("crypto-js");
+var key = 'sfsdfsdf44242sdfds34224dfsfsf34324gdfgdfgd3sdfsdfsdf23sfsdfsdfsdfsffg23@sdf@@!£"$%^&*&fg££$%££@@%$$%£$%"$%fd';
 
 router.get('/', function(req, res, next) {
-    var ciphertext = CryptoJS.AES.encrypt("Hello my name is samuel", 'secret key 123');
-    var bytes = CryptoJS.AES.decrypt(ciphertext.toString(), 'secret key 123');
-    var plaintext = bytes.toString(CryptoJS.enc.Utf8);
-    console.log(plaintext);
+    encrpytData("hello my name is sam");
     res.render('sign_up');
 });
 
+function encrpytData(data){
+    console.log(data + "working here");
+    var ciphertext = CryptoJS.AES.encrypt(data, key);
+    console.log(data + "working here 2");
+    return ciphertext.toString()
+}
+
+function decrpytData(ciphertext){
+    var bytes = CryptoJS.AES.decrypt(ciphertext, key);
+    var plaintext = bytes.toString(CryptoJS.enc.Utf8);
+    return plaintext;
+}
 
 // router.post('/auth',function(req,res,next){
 //     console.log(req);
@@ -19,23 +29,21 @@ let db = require('../dbconnection');
 
 
 router.post('/create', function(req, res, next) {
-
+    console.log("in create");
     // store all the user input data
     const userDetails = req.body;
-    var firstName = userDetails["first-name"];
-    var lastName = userDetails["last-name"];
-    var phoneNumber = userDetails["phone-number"];
-    var email = userDetails["email"];
-    var username = userDetails["username"];
-    var password = userDetails["password"];
-    var univeristy = userDetails["univeristy"];
-    var studentID = userDetails["student-ID"];
-
+    var firstName = encrpytData(userDetails["first-name"]);
+    var lastName = encrpytData(userDetails["last-name"]);
+    var phoneNumber = encrpytData(userDetails["phone-number"]);
+    var email = encrpytData(userDetails["email"]);
+    var username = encrpytData(userDetails["username"]);
+    var password = encrpytData(userDetails["password"]);
+    var univeristy = encrpytData(userDetails["univeristy"]);
+    var studentID = encrpytData(userDetails["student-ID"]);
     var sql = `INSERT INTO Customers (First_Name ,Last_Name ,Phone_Number ,Email ,Registration_Date ,Username, Password ,Customer_Account_Type_ID ,University_Name,Student_ID) VALUES('${firstName}','${lastName}','${phoneNumber}','${email}',NOW(),'${username}','${password}',1,'${univeristy}','${studentID}')`;
     db.query(sql);
     let valid = "true";
     res.redirect('/sign_up?valid="'+valid+'"');
-    db.endConnection();
 });
 
 module.exports = router;
