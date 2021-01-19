5fb6df10d6529fac53104e8e2a3689e5bbaaea3c
b9005890 <nafaykamran@quranchannel.com>
2021-01-19 17:12:30 +0000

    Fixed encryptData and decryptData functions

--- routes/sign_up.js
+++ routes/sign_up.js
@@ -4,11 +4,11 @@ var CryptoJS = require("crypto-js");
 var key = 'sfsdfsdf44242sdfds34224dfsfsf34324gdfgdfgd3sdfsdfsdf23sfsdfsdfsdfsffg23@sdf@@!£"$%^&*&fg££$%££@@%$$%£$%"$%fd';
 
 router.get('/', function(req, res, next) {
-    encrpytData("hello my name is sam");
+    encryptData("hello my name is sam");
     res.render('sign_up');
 });
 
-function encrpytData(data){
+function encryptData(data){
     console.log(data + "working here");
     var ciphertext = CryptoJS.AES.encrypt(data, key);
     console.log(data + "working here 2");
@@ -32,14 +32,14 @@ router.post('/create', function(req, res, next) {
     console.log("in create");
     // store all the user input data
     const userDetails = req.body;
-    var firstName = encrpytData(userDetails["first-name"]);
-    var lastName = encrpytData(userDetails["last-name"]);
-    var phoneNumber = encrpytData(userDetails["phone-number"]);
-    var email = encrpytData(userDetails["email"]);
-    var username = encrpytData(userDetails["username"]);
-    var password = encrpytData(userDetails["password"]);
-    var univeristy = encrpytData(userDetails["univeristy"]);
-    var studentID = encrpytData(userDetails["student-ID"]);
+    var firstName = encryptData(userDetails["first-name"]);
+    var lastName = encryptData(userDetails["last-name"]);
+    var phoneNumber = encryptData(userDetails["phone-number"]);
+    var email = encryptData(userDetails["email"]);
+    var username = encryptData(userDetails["username"]);
+    var password = encryptData(userDetails["password"]);
+    var univeristy = encryptData(userDetails["univeristy"]);
+    var studentID = encryptData(userDetails["student-ID"]);
     var sql = `INSERT INTO Customers (First_Name ,Last_Name ,Phone_Number ,Email ,Registration_Date ,Username, Password ,Customer_Account_Type_ID ,University_Name,Student_ID) VALUES('${firstName}','${lastName}','${phoneNumber}','${email}',NOW(),'${username}','${password}',1,'${univeristy}','${studentID}')`;
     db.query(sql);
     let valid = "true";
