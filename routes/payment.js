76429ff10b6a382613134a52d4e69d598d0b9731
b9005890 <nafaykamran@quranchannel.com>
2021-01-19 17:24:47 +0000

    Added SQL statement to payment

--- routes/payment.js
+++ routes/payment.js
@@ -16,9 +16,10 @@ router.get('/', function(req, res, next) {
     res.render('payment', { title: 'Payment' });
 });
 
-
-
 router.post('/createPayment', function(req, res, next) {
+
+    var sql = `INSERT INTO Transfer_Information (Transfer_Description, Amount_Transferred, Date_Of_Transfer)
+                VALUES (1,1,1);`;
 
 });
 
