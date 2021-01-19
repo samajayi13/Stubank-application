40480a3d9f8a9972a78876a39abd222f0a525f59
b9005890 <nafaykamran@quranchannel.com>
2021-01-19 17:06:24 +0000

    Redirect payment page to login page if not logged in

--- routes/payment.js
+++ routes/payment.js
@@ -2,6 +2,15 @@ var express = require('express');
 var router = express.Router();
 let db = require('../dbconnection');
 
+// if not logged in, doesn't display account page
+const redirectToLogin = (req, res, next) => {
+    if (!req.session.username) {
+        res.redirect('/login')
+    } else {
+        next()
+    }
+}
+
 /* GET payment page. */
 router.get('/', function(req, res, next) {
     res.render('payment', { title: 'Payment' });
