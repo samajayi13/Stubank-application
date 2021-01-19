a32441e054675894bf09960d100279498e1b8283
Samuel Ajayi <ohajayi13@gmail.com>
2021-01-19 18:39:13 +0000

    account details

--- public/js/account.js
+++ public/js/account.js
@@ -1,6 +1,6 @@
 // let userID = req.session.customerID;
-let userID = parseInt(document.querySelector(".userID").innerText);
-
+let userID = parseInt(document.querySelector(".userID").innerText.substr(3));
+console.log(userID)
 
 getAccounts(userID);
 function getAccounts(userID){
@@ -12,6 +12,7 @@ function getAccounts(userID){
     })
         .then(function(response) {
             //what runs after the request has been made and successfully returned
+            console.log(response.data.accountData);
             addAccounts(response.data.accountData)
             console.log("success1");
         })
@@ -25,38 +26,49 @@ function addAccounts(accountData){
         // name, sort code+acc numer, curr balance, avail balance, Action
         var accountDetails = accountData[i].Account_Name+"\n"+ accountData[i].Sort_Code+" "+accountData[i].Account_Number;
         var currentBalance = accountData[i].Current_Balance;
-        var action = "button.btn.btn-primary(type='button') See Transfers";
+        var availableBalance = parseInt(accountData[i].Current_Balance)+ 1500;//do maths here to calc avaible balance
+        var btn = document.createElement("button");
+        btn.innerText = "See Transfers";
+        btn.classList.add("btn","btn-primary");
+        console.log(btn);
 
-        addAccountRow(accountDetails, currentBalance, action);
+
+        addAccountRow(accountDetails, currentBalance, availableBalance,btn);
     }
     console.log("success2");
 }
 
-async  function addAccountRow(accountDetails, currentBalance, action){
+
+//tr
+//    th(scope='row')
+//        | eSaver
+//        br
+//        | 09-01-26 12345678
+//    td &pound; 10,123.00
+//    td &pound; 10,123.00
+//    td
+//        button.btn.btn-primary(type='button') See Transfers
+
+async  function addAccountRow(accountDetails, currentBalance,availableBalance, action){
     // we will insert data into table here
     var tbody = document.querySelector(".table-body");
 
     var tr = document.createElement("tr");
-    var trPounds = document.createElement("td &pound;")
     var th = document.createElement("th");
-    var accountDetailsElm = document.createElement("td")
-    var currentBalanceElm = document.createElement("td")
-    var availableBalanceElm = document.createElement("td")
-    var actionElm = document.createElement("td")
+    var currentBalanceTd = document.createElement("td");
+    var availableBalanceTd = document.createElement("td");
+    var btnTd = document.createElement("td");
 
     th.scope = "row";
+    th.innerText = accountDetails;
+    currentBalanceTd.innerText = "£" + currentBalance;
+    availableBalanceTd.innerText = "£" + availableBalance.toString();
+    btnTd.appendChild(action);
 
-    accountDetailsElm.innerText = accountDetails;
-    currentBalanceElm.innerText = currentBalance;
-    availableBalanceElm.innerText = currentBalance;
-    actionElm.innerText = action;
-
-
-    tr.appendChild(accountDetailsElm);
     tr.appendChild(th);
-    trPounds.appendChild(currentBalanceElm);
-    trPounds.appendChild(availableBalanceElm);
-    tr.appendChild(actionElm);
+    tr.appendChild(currentBalanceTd);
+    tr.appendChild(availableBalanceTd);
+    tr.appendChild(btnTd);
     tbody.appendChild(tr);
 
     console.log("success3");
