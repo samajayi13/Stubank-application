d4e3051eee89c2a9cee868232c849c7293e6c63b
b9005890 <nafaykamran@quranchannel.com>
2021-01-21 17:30:06 +0000

    Made payment form retrieve accounts from database for current user
    Moved unnecessary code from pug file to a JavaScript file

--- routes/payment.js
+++ routes/payment.js
@@ -26,4 +26,16 @@ router.post('/createPayment', function(req, res, next) {
     res.redirect('/payment?valid="'+valid+'"');
 });
 
+router.get('/getUserAccounts', function(req, res, next) {
+    var userID = req.query.ID;
+    var sql =  `SELECT Account_Name FROM Bank_Accounts WHERE Customer_ID = ${userID}`;
+
+    db.query(sql,function(error,results,fields){
+        if (error) throw error;
+        console.log(results);
+        res.send({userAccounts : results });
+    });
+});
+
+
 module.exports = router;
