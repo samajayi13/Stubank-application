856e725d4ebb29a6a69c63ee65c0a057e6f7c7f4
Ivelina <i.kiryakova1@newcastle.ac.uk>
2021-01-25 17:10:38 +0200

    Added pdfkit code for e-documents

--- routes/e-documents.js
+++ routes/e-documents.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../dbconnection');
+const PDFDocument = require('pdfkit');
+var fs = require('file-system');
+
 
 // if not logged in, doesn't display e-documents page
 const redirectToLogin = (req, res, next) => {
@@ -10,12 +13,6 @@ const redirectToLogin = (req, res, next) => {
         next();
     }
 }
-
-router.get('/download', function (req,res, next) {
-    var filepath = "routes/generatedStatements/SALVA.pdf";
-    console.log("Test");
-    res.download('generatedStatements/SALVA.pdf');
-});
 
 /* GET e-documents page. */
 router.get('/', redirectToLogin, function(req, res, next) {
@@ -75,10 +72,58 @@ router.get('/getStatementInfo', function(req, res, next) {
 
 `;
 
-    db.query(sql,function(error,results,fields){
-        if (error) throw error;
-        res.send({results : results });
-    });
+    // db.query(sql,function(error,results,fields){
+    //     if (error) throw error;
+    //     res.send({results : results });
+    // });
+
+    // create a document and pipe to a blob
+    var doc = new PDFDocument();
+
+
+// draw some text
+    doc.fontSize(25).text('Here is some vector graphics...', 100, 80);
+
+// some vector graphics
+    doc
+        .save()
+        .moveTo(100, 150)
+        .lineTo(100, 250)
+        .lineTo(200, 250)
+        .fill('#FF3300');
+
+    doc.circle(280, 200, 50).fill('#6600FF');
+
+// an SVG path
+    doc
+        .scale(0.6)
+        .translate(470, 130)
+        .path('M 250,75 L 323,301 131,161 369,161 177,301 z')
+        .fill('red', 'even-odd')
+        .restore();
+
+// and some justified text wrapped into columns
+    doc
+        .text('And here is some wrapped text...', 100, 300)
+        .font('Times-Roman', 13)
+        .moveDown()
+        .text(lorem, {
+            width: 412,
+            align: 'justify',
+            indent: 30,
+            columns: 2,
+            height: 300,
+            ellipsis: true
+        });
+
+    doc.end();
+    res.send(doc.toString('base64'));
+    console.log(res);
+
+    // fs.readFile(doc, function (err,data){
+    //     res.contentType("application/pdf");
+    //     res.send(data);
+    // });
 });
 
 module.exports = router;
