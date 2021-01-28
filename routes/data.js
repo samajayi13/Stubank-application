4f9caebf85c2391b514e54d3547063092efc1b7d
issy wallwork <b8071768@newcastle.ac.uk>
2021-01-28 13:35:00 +0000

    comments added to machine learning files

--- routes/data.js
+++ routes/data.js
@@ -1,8 +1,12 @@
 const tf = require('@tensorflow/tfjs');
 
+// creates a dataset for machine learning from a given CSV file
 async function createDataset(csvPath) {
+  // uses tensorflow function to access columns of given CSV file
   const dataset = tf.data.csv(csvPath, {hasHeader: true, columnConfigs: {'Category': {isLabel: true}}});
+  // sets the numOfColumns to one less
   const numOfColumns = (await dataset.columnNames()).length - 1;
+  // returns the dataset of features to labels and numOfColumns - doesn't function correctly - explained in test tables
   return {
         dataset: dataset.map(row => {
         const rawFeatures = row['xs'];
--- routes/train.js
+++ routes/train.js
@@ -9,38 +9,26 @@ router.get('/', function(req, res, next) {
     res.render('payment', { title: 'Payment' });
 });
 
+// runs the machine learning prediction
 router.get('/run', async function(req, res, next) {
+    // calling the createDataset function from data.js
     const datasetObj = await createDataset('file://' + csvPath);
-    console.log(datasetObj);
-    const model = createModel([datasetObj.numOfColumns]);
 
-//     const batchSize = 10;
-//     const savePath = 'file://routes/machineLearning/trainedModel';
-//
-//     const trainBatches = Math.floor(50 / batchSize);
-//     const dataset = datasetObj.dataset.shuffle(50).batch(batchSize);
-//     const trainDataset = dataset.take(trainBatches);
-//     const validationDataset = dataset.skip(trainBatches);
-//
-//     await model.fitDataset(trainDataset, {epochs: 10, validationData: validationDataset});
-//
-//     await model.save(savePath);
-//
-//     const loadedModel = await tf.loadLayersModel(savePath + '/model.json');
-//     const result = loadedModel.predict(tf.tensor2d([20], [1, 1])).dataSync();
+    // calling the createModel function from model.js
+    const model = createModel([datasetObj.numOfColumns]);
 
     // synthetic data for training
     const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
     const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
 
+    // calculating the prediction
     var prediction = model.predict(tf.tensor2d([20], [1, 1])).toInt().dataSync();
 
     var categories = ['Charity', 'Eating Out', 'Entertainment', 'General', 'Gift', 'Groceries', 'Family/Friends',
     'Personal Care', 'Shopping', 'Transport']
 
+    // mapping the prediction to its spending category
     var text = categories[prediction[0]];
-
-    console.log(text);
 
     res.send({text : text });
 });
